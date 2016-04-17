9d33f9251a329856d7d8ec11a69e3eaac587fc12
Annihilatrix <annihilatrix@katie.codes>
2016-04-17 07:38:04 -0700

    Classes now in single skills.py file

---
+++ skills-oo/skills.py
@@ -0,0 +1,25 @@
+class Student(object):
+
+    def __init__(self, first_name, last_name, address):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+   
+
+class Question(object):
+    question = self.question
+    correct_answer = self.correct_answer
+
+    def ask_and_evaluate():
+        
+
+class Exam(self, question, correct_answer):
+    def __init__(self):
+        questions = []
+
+    def add_question(self):
+
+    def administer(self):
+        # for question in Exam
+        # ask_and_evaluate
+        return score
