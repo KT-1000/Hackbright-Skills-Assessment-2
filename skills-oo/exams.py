b54e2ccae10ea59b8cf477ae72173d804d515e75
Annihilatrix <annihilatrix@katie.codes>
2016-04-17 07:37:36 -0700

    All classes in one skills.py file.

--- skills-oo/exams.py
+++ skills-oo/exams.py
@@ -1,10 +1,19 @@
-def Question(object):
+class Student(object):
+
+    def __init__(self, first_name, last_name, address):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+   
+
+class Question(object):
     question = self.question
     correct_answer = self.correct_answer
 
     def ask_and_evaluate():
+        
 
-def Exam(self, question, correct_answer):
+class Exam(self, question, correct_answer):
     def __init__(self):
         questions = []
 
