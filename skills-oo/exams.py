fdd0867c83b8b1a1edd27f3e2a25339d6ffa3956
Annihilatrix <annihilatrix@katie.codes>
2016-04-17 01:23:42 -0700

    Brief outline of project as described in instructions.txt

---
+++ skills-oo/exams.py
@@ -0,0 +1,16 @@
+def Question(object):
+    question = self.question
+    correct_answer = self.correct_answer
+
+    def ask_and_evaluate():
+
+def Exam(self, question, correct_answer):
+    def __init__(self):
+        questions = []
+
+    def add_question(self):
+
+    def administer(self):
+        # for question in Exam
+        # ask_and_evaluate
+        return score
---
+++ skills-oo/students.py
@@ -0,0 +1,7 @@
+def Student(self, object):
+    first_name = self.first_name
+    last_name = self.last_name
+    address = self.address
+
+    def __init__(self, first_name, last_name, address):
+        
